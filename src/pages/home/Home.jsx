774075ac2e7c85424de72a6dbb4434b0287c758f
MOHAMMAD WASIF <erwasifmohd786@gmail.com>
2024-05-06 11:00:51 +0530

    Initial commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,10 @@
 import "./home.css";
 
 const Home = () => {
-  const signInWithGoogle = () => {
-    console.log("sign in handled");
-  };
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <button
-        onClick={signInWithGoogle}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded"
-      >
-        Sign in with Google
-      </button>
+    <div >
+      hello
     </div>
   );
 };
